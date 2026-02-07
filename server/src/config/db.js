2e749ae4490346a6fca5a9bc3ae445fd3b0fadaf
Navadeep Bommarapu <bommarapunavadeep@gmail.com>
2026-02-07 19:59:54 +0530

    modified db file

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,27 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        protocol: "postgres",
         logging: false,
-    }
-);
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: "postgres", // default to postgres
+            logging: false,
+        }
+    );
 
 module.exports = sequelize;
